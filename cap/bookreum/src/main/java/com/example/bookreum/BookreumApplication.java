c620f0005babf32278826379ad10382d79a204ee
Lydia.Y <110841277+LydiaLog@users.noreply.github.com>
2025-04-08 20:10:17 +0900

    sts settings
    sts 기본 세팅

---
+++ cap/bookreum/src/main/java/com/example/bookreum/BookreumApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookreum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookreumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookreumApplication.class, args);
+	}
+
+}
---
+++ cap/bookreum/src/test/java/com/example/bookreum/BookreumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookreum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookreumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
