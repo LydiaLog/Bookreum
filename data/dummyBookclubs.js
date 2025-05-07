b6a712dae746660aea754cc227cffa30362f411f
Yerin <84303627+yerin08@users.noreply.github.com>
2025-05-07 12:43:41 +0900

    북그러움 기타 폴더 (로고 이미지, 더미데이터...)

---
+++ data/dummyBookclubs.js
@@ -0,0 +1,30 @@
+const dummyBookclubs = [
+    {
+      id: 1,
+      title: '월간 판타지 독서회',
+      book: '해리 포터와 불사조 기사단',
+      author: 'J.K. 롤링',
+      nickname: '또리왕자',
+      date: '2025.05.31',
+      status: 'open',
+    },
+    {
+      id: 2,
+      title: '철학 고전 모임',
+      book: '소크라테스의 변명',
+      author: '플라톤',
+      nickname: '송만두',
+      date: '2025.05.13',
+      status: 'open',
+    },
+    {
+        id: 3,
+        title: '추리소설 애호가',
+        book: '그리고 아무도 없었다',
+        author: '아가사 크리스티',
+        nickname: '이것모에여~?',
+        date: '2025.04.27',
+        status: 'closed',
+    },    
+];
+export default dummyBookclubs;  
