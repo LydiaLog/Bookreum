8cf3214f2c103ac77b8861a55f4cf7f6d161f231
Yerin <84303627+yerin08@users.noreply.github.com>
2025-05-24 15:37:05 +0900

    북그러움 더미 데이터 (북로그, 북클럽) 폴더

--- data/dummyBookclubs.js
+++ data/dummyBookclubs.js
@@ -1,28 +1,35 @@
+import harrypotter from '../assets/harrypotter.png';
+import socrates from '../assets/socrates.png';
+import puppy from '../assets/puppy.jpg';
+
 const dummyBookclubs = [
   {
     id: 1,
+    image: harrypotter,
     title: '월간 판타지 독서회',
     description: '판타지 소설을 함께 읽고 토론하는 모임입니다.',
     book: '해리 포터와 불사조 기사단',
     author: 'J.K. 롤링',
     nickname: '또리왕자',
-    date: '2025-05-31',
+    date: '2025-06-08',
     capacity: 5,
     currentMembers: 4
   },
   {
     id: 2,
+    image: socrates,
     title: '철학 고전 모임',
     description: '철학 고전을 함께 읽고 생각을 나누는 모임.',
     book: '소크라테스의 변명',
     author: '플라톤',
     nickname: '송만두',
-    date: '2025-05-17',
+    date: '2025-05-31',
     capacity: 4,
     currentMembers: 1
   },
   {
     id: 3,
+    image: puppy,
     title: '추리소설 애호가',
     description: '추리소설을 좋아하는 사람들의 모임, 매주 한 권씩.',
     book: '그리고 아무도 없었다',
